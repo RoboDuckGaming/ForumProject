432fb41f312f80c751b3c90db37cbe27a37fb3b0
oSxpreme <73104883+oSxpreme@users.noreply.github.com>
2021-05-31 11:58:24 +0200

    Contact form
    31-05-2021

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+include("header.php");
+?>
+<html lang="en">
+<body>
+<section class="contact-form">
+    <form id="contact-form" method="post" action="contactAction.php">
+        <section>
+            <input name="name" type="text" class="form-control" placeholder="Username" required>
+            <br>
+        </section>
+        <section>
+            <input name="email" type="email" class="form-control" placeholder="Email" required>
+        </section>
+        <section>
+            <textarea name="message" class="form-control" placeholder="Message" required></textarea><br>
+            <input type="submit" class="form-control submit" value="SEND">
+        </section>
+    </form>
+</section>
+</body>
+</html>
---
+++ contactAction.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'peter.benninga2@gmail.com';
+
+$email_subject = "RocketDuckGaming message";
+
+$email_body = "User Name: $name.\n" .
+    "User Email: $visitor_email.\n" .
+    "User Message: $message.\n";
+
+$to = "p.benninga@student.alfa-college.nl";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: contact.php");
+
+
