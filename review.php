dbaf83023a4a4a5287459312b741cfff663b21be
oSxpreme <73104883+oSxpreme@users.noreply.github.com>
2021-06-04 15:57:02 +0200

    04-06-2021

--- review.php
+++ review.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Posts</title>
+    <title>Review</title>
 </head>
 <body>
 
 <?php
 $connect = mysqli_connect("localhost", "root", "");
 mysqli_select_db($connect,"rocketduckgaming");
-//Database connection
 
-$sql = "SELECT * FROM games";
+$gameID = $_GET['gameID'];
+$sql = "SELECT * FROM reviews WHERE reviewGameID = '$gameID';";
+
 $result = mysqli_query($connect, $sql);
 $resultCheck = mysqli_num_rows($result);
 
+
 if ($resultCheck > 0){
     while($row = mysqli_fetch_assoc($result)){
-        $gameID = $row['gameID'];
-        echo "<strong>" . $row['gameTitle'] . "</strong>, ";
-        echo $row['gameDescription'] . "<br>" . "<button onclick=window.location='review.php?gameID=$gameID'>Review</button>";
-        echo "<hr> <br>";
+        echo "<strong>" . $row['reviewTitle'] . "</strong><br>";
+        echo $row['reviewText'];
+        echo "<hr><br>";
     }
 }else {
     echo "Something went wrong";
 }
 ?>
-
 </body>
 </html>
