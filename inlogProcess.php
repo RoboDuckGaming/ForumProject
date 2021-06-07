7cd992ba723389bee1f13559a6bb1ad876a036a1
oSxpreme <73104883+oSxpreme@users.noreply.github.com>
2021-06-07 10:22:43 +0200

    07-06-21

--- inlogProcess.php
+++ inlogProcess.php
@@ -3,20 +3,20 @@
 $connect = mysqli_connect("localhost", "root", "");
 mysqli_select_db($connect,"rocketduckgaming");
 
-// username en wachtwoord ophalen uit het formulier in het bestand index.php
+// username en wachtwoord ophalen uit het formulier in het bestand login.php
 $username = $_POST['user'];
 $password = $_POST['pass'];
 
 // controle op lege velden
 
  if($username == "" && $password ==""){
-     header("Location: index.php?error=1");
+     header("Location: login.php?error=1");
      die();
  }elseif($username == "") {
-     header("Location: index.php?error=2");
+     header("Location: login.php?error=2");
      die();
  }elseif($password == ""){
-     header("Location: index.php?error=3");
+     header("Location: login.php?error=3");
      die();
  }
 // om sql injecties te voorkomen
@@ -37,7 +37,7 @@ $row = mysqli_fetch_array ($result);
     if ($row['userName'] == $username && $row['userPassword'] == $password ){
     echo "Welcome ".$row['userName'];
     }elseif($row['userName'] != $username || $row['userPassword'] != $password){
-        header("location: index.php?error=4");
+        header("location: login.php?error=4");
         die();
     }
     else {
