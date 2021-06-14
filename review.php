875c72e742bd86282866df92de721dbdcb4d8871
oSxpreme <73104883+oSxpreme@users.noreply.github.com>
2021-06-14 14:00:58 +0200

    14-06-2021

--- review.php
+++ review.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION["username"]) && $_SESSION["status"]!="actief"){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
-<body style="background-color: white;">
+<body>
 
 <?php
 $connect = mysqli_connect("localhost", "root", "");
