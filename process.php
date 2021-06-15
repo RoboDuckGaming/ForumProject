5cf84e011adc902fa363b3917c186818e8ef2993
Dilshad22179 <alsmoqidilshad@gmail.com>
2021-06-15 10:40:20 +0200

    a borro

---
+++ process.php
@@ -0,0 +1,22 @@
+<?php
+require_once('config.php');
+?>
+<?php
+
+if(isset($_POST)){
+
+	$username 		= $_POST['username'];
+	$email 			= $_POST['email'];
+	$wachtwoord 		= sha1($_POST['wachtwoord']);
+
+		$sql = "INSERT INTO users (username, email, wachtwoord ) VALUES(?,?,?,?,?)";
+		$stmtinsert = $db->prepare($sql);
+		$result = $stmtinsert->execute([$username, $email, $wachtwoord]);
+		if($result){
+			echo 'Successfully saved.';
+		}else{
+			echo 'There were erros while saving the data.';
+		}
+}else{
+	echo 'No data';
+}
